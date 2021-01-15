42bb996404ec9b357342fbd18b5bd71d8cde370d
Chris Vega <chrisvega@simple.com>
2021-01-15 14:26:32 -0800

    Implement API endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from ..lib.fibonacci import fibonacci
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    pass
+    return {'message': 'hello, world'}
+
+
+@app.get('/fibonacci/{num}')
+async def calculate_fibonacci(num: int, response: Response):
+    try:
+        return {
+            'number': num,
+            'result': fibonacci(num)
+        }
+    except ValueError as value_error:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {'detail': str(value_error)}
--- src/tests/integration/test_main.py
+++ src/tests/integration/test_main.py
@@ -1,8 +1,52 @@
-from ..app.main import app
+from ...app.main import app
 from fastapi.testclient import TestClient
+import pytest
 
 client = TestClient(app)
 
 
 def test_get_root():
-    pass
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'hello, world'}
+
+
+@pytest.mark.parametrize('number, result', [
+    (0, 1),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (20, 6765),
+])
+def test_calculate_fibonacci(number, result):
+    response = client.get(f'/fibonacci/{number}')
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'number': number,
+        'result': result
+    }
+
+
+def test_fibonacci_less_than():
+    num = -1
+    response = client.get(f'/fibonacci/{num}')
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': f'{num} less than min of 0'}
+
+
+def test_fibonacci_greater_than():
+    num = 21
+    response = client.get(f'/fibonacci/{num}')
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': f'{num} greater than max of 20'}
+
+
+def test_not_found():
+    response = client.get('/nonexistent-endpoint')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
